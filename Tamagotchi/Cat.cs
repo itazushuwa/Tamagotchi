24a7100071c1d4962410757ca741e84b9a46cd5e
SuvorovDA <SuvorovDA@411D-12>
2023-03-04 12:13:01 +0700

    Добавьте файлы проекта.

---
+++ Tamagotchi/Cat.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Tamagotchi;
+
+namespace Tamagotchi
+
+{
+    struct Fruit
+    {
+        public string type;
+        public int health;
+    }
+
+
+    class Cat
+    {
+        public void Meow()
+        {
+            Console.WriteLine("Meow!");
+        }
+        public void Eat(Fruit f)
+        {
+            Console.WriteLine($"Cat eats {f.type}. {f.health} hp.\n");
+        }
+    }
+}
+
---
+++ Tamagotchi/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Tamagotchi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Cat cat = new Cat();
+            cat.Meow();
+           Fruit FruitBanana = new Fruit {type = "banana", health = 228 };
+            cat.Eat(FruitBanana);
+            Fruit FruitMango = new Fruit { type = "mango", health = 400 };
+            cat.Eat(FruitMango);
+        }
+    }
+}
